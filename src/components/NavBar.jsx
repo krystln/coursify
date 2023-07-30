5d08a6e11086d805b2de68a842011a21735b70c2
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2023-07-30 14:51:39 +0530

    It all begins!

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+        <img src="" alt="Logo" />
+        <button>Sign In</button>
+        <button>Sign Up</button>
+    </div>
+  )
+}
+
+export default NavBar   
