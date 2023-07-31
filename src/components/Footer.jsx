3233a079f6c5422fecf1457b8b466d7fed3197c9
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2023-07-31 21:53:36 +0530

    Footer Created, temp logos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 
 const Links = ({ head, linkList }) => {
   const link = linkList.map((link) => {
@@ -6,37 +6,65 @@ const Links = ({ head, linkList }) => {
       <a
         href="/"
         onClick={(e) => e.preventDefault()}
-        className="font-light hover:text-blue-900 hover:underline hover:underline-offset-2">
+        className="font-light hover:text-blue-900 hover:underline hover:underline-offset-2"
+      >
         {link}
       </a>
-    );
-  });
+    )
+  })
 
   return (
-    <div className="text-lg font-bold flex flex-col">
+    <div className="flex flex-col gap-2 text-lg font-bold">
       {head}
       {link}
     </div>
-  );
-};
+  )
+}
 
 const Footer = () => {
   return (
-    <div className="w-full border-t border-black bg-gray-100 flex items-center justify-between px-10">
-      <div className="py-4">
-        <div className="font-serif font-black text-4xl">Recruitify</div>
-        <div className="flex">
-          <img src="./assets/vite.svg" alt="Linkedin logo" />
-          <img src="./assets/vite.svg" alt="Twitter logo" />
+    <div className="bg-gray-200">
+      <div className="flex w-full items-start justify-between px-10">
+        <div className="pt-16">
+          <div className="font-serif text-4xl font-black">Recruitify</div>
+          <div className="flex gap-2">
+            <img src="./public/vite.svg" alt="Linkedin logo" />
+            <img src="./public/vite.svg" alt="Twitter logo" />
+          </div>
+        </div>
+        <div className="flex gap-10 py-16 pr-20">
+          <Links
+            head="For Recruiters"
+            linkList={["Overview", "Our Tooling", "Hire Talent", "Pricing"]}
+          />
+          <Links
+            head="For Candidates"
+            linkList={[
+              "Overview",
+              "Remote",
+              "Web3 Jobs",
+              "Data Jobs",
+              "Profile Reviewer",
+              "Company List",
+            ]}
+          />
+          <Links
+            head="About Company"
+            linkList={[
+              "About",
+              "Contact",
+              "Blog",
+              "Terms & Conditions",
+              "Cookies",
+            ]}
+          />
         </div>
       </div>
-      <div className="flex gap-10">
-        <Links head="For Recruiters" linkList={["Overview", "data"]} />
-        <Links head="For Candidates" linkList={["Overview"]} />
-        <Links head="About Company" linkList={[]} />
+      <div className="px-10 pb-5 text-lg text-zinc-500">
+        Copyright © 2023 Recruitify. All rights reserved.
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
