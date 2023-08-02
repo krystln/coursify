d8fe5837e2bc673270656b1e4e5d91e9cb0adacb
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2023-08-02 23:34:06 +0530

    signin PopUp

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -3,8 +3,17 @@ import React from "react"
 const PopUp = ({ children, isShown, togglePopUp }) => {
   return isShown ? (
     <div className="fixed flex h-full w-full items-center justify-center bg-[rgba(0,0,0,0.5)]">
-      <div className="relative h-fit w-fit bg-white">
-        <button onClick={togglePopUp}>Close</button>
+      <div className="relative h-fit w-[20rem] overflow-hidden rounded-md border border-zinc-500 bg-white">
+        <button
+          onClick={togglePopUp}
+          className="absolute right-0 h-0 px-5 py-5 text-zinc-400 active:text-black"
+        >
+          <img
+            src="/crossIcon.svg"
+            alt="Close"
+            className="h-5 w-5 hover:scale-125 active:scale-100"
+          />
+        </button>
         {children}
       </div>
     </div>
--- src/components/subComps/SignIn.jsx
+++ src/components/subComps/SignIn.jsx
@@ -4,18 +4,36 @@ const SignIn = () => {
   document.getElementById("root").style.filter = "blur(20px);"
 
   return (
-    <div className="bg-white">
-      <h1>Sign In</h1>
-      <p></p>
-      <div>
-        <form className="flex flex-col">
-          <input type="text" placeholder="Email" />
-          <input type="text" placeholder="Password" />
-          <button>Sign In</button>
+    <div className="h-fit w-fit bg-white px-5 py-5">
+      <h1 className="text-xl font-bold">Sign In</h1>
+      <p className="text-zinc-500">Get all the features unlocked</p>
+      <div className=" w-fulflex flex-col">
+        <form className="flex flex-col gap-2">
+          <input
+            type="text"
+            placeholder="Email"
+            className="rounded-sm border border-zinc-300 px-2 py-1"
+          />
+          <input
+            type="text"
+            placeholder="Password"
+            className="rounded-sm border border-zinc-300 px-2 py-1"
+          />
+          <button className="rounded-sm bg-black py-1 text-white">
+            Sign In
+          </button>
         </form>
-        <hr />
-        <button>Sign In With Google</button>
-        <button>Sign In With Github</button>
+        <hr className="mx-[auto] my-2 w-4/5 border border-zinc-200" />
+        <button className="mb-2 w-full rounded-sm border border-zinc-300 px-2 py-1 hover:bg-zinc-200 active:bg-white">
+          Sign In With{" "}
+          <img src="/google.svg" alt="" className="mb-1 inline h-5 w-5" />
+          Google
+        </button>
+        <button className="w-full rounded-sm border border-zinc-300 px-2 py-1 hover:bg-zinc-200 active:bg-white">
+          Sign In With{" "}
+          <img src="/github.svg" alt="" className="mb-1 inline h-5 w-5 " />
+          Github
+        </button>
       </div>
     </div>
   )
