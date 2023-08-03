c0720d74f21e709602c7f8bb45c18d31ed3f5aa7
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2023-08-03 17:26:08 +0530

    nav mobile done & fin...

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,25 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 const NavBar = () => {
   function scrollTo(event) {
     event.preventDefault()
     document.scroll("/")
+  }
+
+  const dropdown = React.useRef(null)
+  const [isOpen, setIsOpen] = React.useState(false)
+  useEffect(() => {
+    if (isOpen) {
+      dropdown.current.classList.add("block")
+      dropdown.current.classList.remove("hidden")
+    } else {
+      dropdown.current.classList.add("hidden")
+      dropdown.current.classList.remove("block")
+    }
+  }, [isOpen])
+
+  function toggleIsOpen() {
+    setIsOpen((prevIsOpen) => !prevIsOpen)
   }
 
   return (
@@ -42,6 +58,43 @@ const NavBar = () => {
         <button className=" hover:delay-50 rounded-md border border-black bg-black px-4 py-1 text-white hover:border-blue-600 hover:bg-blue-600 hover:ease-in-out">
           Sign Up
         </button>
+      </div>
+      <div className="min-[800px]:hidden">
+        <button
+          className="rounded-md active:bg-zinc-200"
+          onClick={toggleIsOpen}
+        >
+          <img src="/menuIcon.svg" alt="" className="h-10 w-10" />
+        </button>
+        <div
+          ref={dropdown}
+          className="absolute right-[2rem] top-[5rem] flex flex-col rounded-b-md bg-white text-xl shadow-lg"
+        >
+          <a
+            href="/"
+            className="hover:delay-50 px-10 py-2 text-lg hover:text-blue-900 hover:underline hover:underline-offset-8 hover:ease-in-out"
+          >
+            Blog
+          </a>
+          <a
+            href="/"
+            className="hover:delay-50 px-10 py-2 text-lg hover:text-blue-900 hover:underline hover:underline-offset-8 hover:ease-in-out"
+          >
+            Integrations
+          </a>
+          <a
+            href="/"
+            className="hover:delay-50 px-10 py-2 text-lg hover:text-blue-900 hover:underline hover:underline-offset-8 hover:ease-in-out"
+          >
+            Pricing
+          </a>
+          <a
+            href="/"
+            className="hover:delay-50 px-10 py-2 text-lg hover:text-blue-900 hover:underline hover:underline-offset-8 hover:ease-in-out"
+          >
+            For Individuals
+          </a>
+        </div>
       </div>
     </div>
   )
